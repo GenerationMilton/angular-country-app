da5b230394e5759569cfbd068de56b402b3430b3
Milton Munoz <livemilton@gmail.com>
2025-06-08 17:09:42 -0500

    Angular CountryApp - :sparkles: part 2 detail of a country

--- src/app/country/pages/country-page/country-page.component.ts
+++ src/app/country/pages/country-page/country-page.component.ts
@@ -2,24 +2,23 @@ import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CountryService } from '../../services/country.service';
 import { rxResource } from '@angular/core/rxjs-interop';
+import { NotFoundComponent } from "../../../shared/components/not-found/not-found.component";
+import { CountryInformationComponent } from "./country-information/country-information.component";
 
 @Component({
   selector: 'country-page',
-  imports: [],
+  imports: [NotFoundComponent, CountryInformationComponent],
   templateUrl: './country-page.component.html',
 
 })
 export class CountryPageComponent {
-
   countryCode = inject(ActivatedRoute).snapshot.params['code'];
   countryService = inject(CountryService);
 
   countryResource = rxResource({
-    params: () => ({ code: this.countryCode }),
-    loader:  ({ params }) => {
-      return  this.countryService.searchCountryByAlphaCode(params.code);
+    request: () => ({ code: this.countryCode }),
+    loader: ({ request }) => {
+      return this.countryService.searchCountryByAlphaCode(request.code);
     },
-  })
-
-
- }
+  });
+}
